37d24f1349f2e5237e966cec8815cfb93e7a1701
Renato Silveira (LT) <renatos@leapthought.co.nz>
2025-05-15 00:51:57 -0300

    - fixed unit test project - fixed a bug on pointInTriangle

--- geometry4SharpTests/math/Triangle3Tests.cs
+++ geometry4SharpTests/math/Triangle3Tests.cs
@@ -20,6 +20,24 @@ namespace geometry4SharpTests
             var isPointOnTri = triangleA.IsPointInTriangle(pointA);
 
             isPointOnTri.Should().BeFalse();
+
+
+            var pointB = new Vector3d(-21.01844597, 3.80000305, -5.53531647);
+
+            triangleA.IsPointInTriangle(pointB).Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsPointInTriangle_PointInside()
+        {
+            var triangleA = new Triangle3d(
+                new Vector3d(-22.03933907, 3.80000305, -6.62411737),
+                new Vector3d(-22.03933907, 3.80000305, 4.11130095),
+                new Vector3d(-10.74934673, 3.80000305, 4.11130095));
+
+            var pointB = new Vector3d(-21.01844597, 3.80000305, -5.53531647);
+
+            triangleA.IsPointInTriangle(pointB).Should().BeTrue();
         }
     }
 }
